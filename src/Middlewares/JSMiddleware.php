ca256b0401a38d9cc772b8ee97f88473456d3246
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-06-05 23:37:27 +0200

    add static load fn

--- src/Middlewares/JSMiddleware.php
+++ src/Middlewares/JSMiddleware.php
@@ -8,7 +8,7 @@ class JSMiddleware implements IMiddleware{
     public static function register(){
         App::use('js_socket',function(){
             try{
-                App::javascript('js_socket', './socket/js.js',[],100000);
+                //App::javascript('js_socket', './socket/js.js',[],100000);
             }catch(\Exception $e){
                 App::set('maintanceMode','on');
                 App::addError($e->getMessage());
