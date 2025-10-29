c82293ddd87b2c37c1477f98feccc56be4a0f7f8
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:29:54 +0100

    Version bump to 1.0.2

--- src/Routes/JS.php
+++ src/Routes/JS.php
@@ -1,22 +1,24 @@
 <?php
+
 namespace Tualo\Office\Socket\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
-class JS implements IRoute{
-    public static function register(){
+class JS extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
 
-        BasicRoute::add('/socket/js.js',function($matches){
-            $path = dirname(dirname(__DIR__)).'';
+        BasicRoute::add('/socket/js.js', function ($matches) {
+            $path = dirname(dirname(__DIR__)) . '';
             $data = [
-                file_get_contents( $path."/src/js/socket.io.js" ),
-                file_get_contents( $path."/src/js/Socket.js" )
+                file_get_contents($path . "/src/js/socket.io.js"),
+                file_get_contents($path . "/src/js/Socket.js")
             ];
-            App::body( implode(';',$data) );
+            App::body(implode(';', $data));
             App::contenttype('javascript/application');
         });
-
     }
 }
